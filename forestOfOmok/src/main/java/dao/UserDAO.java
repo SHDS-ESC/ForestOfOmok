cb7266e93938093a3fc1e29f137230dca1a06a6b
선우 <dlsundn46@naver.com>
2025-05-27 21:09:58 +0900

    feat : user클래스에 필드 추가

--- forestOfOmok/src/main/java/dao/UserDAO.java
+++ forestOfOmok/src/main/java/dao/UserDAO.java
@@ -1,5 +1,16 @@
 package dao;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
+public class UserDAO {
+	
+	private PreparedStatement pstmt;
+	private Statement stmt;
+	private Connection con;
+	private String driver = "oracle.jdbc.OracleDriver";
+	private String url = "jdbc:oracle:thin:@192.168.0.93:1521/xe";
+	private String user = "omok";
+	private String pwd = "test1234";
 }
