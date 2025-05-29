59f5e62b56b0fdc69aab4e4d1d061516dc49d646
jiwon <jwp0329@naver.com>
2025-05-29 21:41:09 +0900

    Feat : UserDao 추가
    Dao rankList, history 부분 추가

--- forestOfOmok/src/main/java/dao/UserDAO2.java
+++ forestOfOmok/src/main/java/dao/UserDAO2.java
@@ -1,5 +1,11 @@
 package dao;
+import java.util.List;
+
+import model.History;
 import model.Profile;
+import model.Rank;
 public interface UserDAO2 {
 	Profile getProfile(String id);
+	List<Rank> getRankList();
+	List<History> getHistoryList(String ownerId);
 }
