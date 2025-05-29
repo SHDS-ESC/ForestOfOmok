0fbbba756f566fff855361a87dedc508ce15b5b1
jiwon <jwp0329@naver.com>
2025-05-29 21:35:05 +0900

    Feat : History, Rank 모델 추가
    전적, 랭크 리스트 조회를 위한 모델 추가

---
+++ forestOfOmok/src/main/java/model/History.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class History {
+	private String imgPath;
+	private String winnerColor;
+	private String time;
+	private String turnCount;
+	private String data;
+	private String winnerName;
+	private String loserName;
+}	
